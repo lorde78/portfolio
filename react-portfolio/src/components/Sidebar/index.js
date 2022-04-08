208914c72f83e2fc5a64cd41f0edb25666f6eb22
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-04-08 08:09:49 +0200

    nav & nav-bar

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -1,7 +1,29 @@
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons'
 
-const Sidebar = () =>{
-    <>Hello from sidebar</>
-}
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className="logo" to='/'>
+        <img src={LogoS} alt="logo"></img>
+        <img className="sub-logo" src={LogoSubtitle} alt="logo"></img>
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+    </div>
+)
+
 
 export default Sidebar
